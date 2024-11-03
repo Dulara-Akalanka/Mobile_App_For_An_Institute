e4987ffbcf89bc8e4b773884090bc86754d8dd91
dulara-akalanka <akalanka0910@gmail.com>
2024-11-03 06:06:30 +0530

    Initial commit

---
+++ app/src/main/java/com/wine/assignment_1/Dashboard.java
@@ -0,0 +1,40 @@
+package com.wine.assignment_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dashboard);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        CircleImageView pp = findViewById(R.id.circleImageView);
+
+
+        pp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Dashboard.this, Profile.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
